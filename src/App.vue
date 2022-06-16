8d5aa018d5ff2c33fbb4ad56257c9cedc4b096e3
itpark <a.m.smyslov@yandex.ru>
2022-06-16 20:18:14 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <component-one/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ComponentOne from "@/components/ComponentOne";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ComponentOne,
   }
 }
 </script>
