79dd88217ae889bb222eda0f9228ff000a6d51b7
itpark <a.m.smyslov@yandex.ru>
2022-06-23 21:58:11 +0300

    add global validate with configure

--- src/components/ComponentAuth.vue
+++ src/components/ComponentAuth.vue
@@ -1,33 +1,37 @@
 <template>
   <main class="form-signin">
-    <form>
-      <h1 class="h3 mb-3 fw-normal">Please sign in
-      </h1>
-
-      <div>
-        <bootstrap-icon icon="alarm-fill" />
-      </div>
+    <Form v-slot="{errors}">
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
       <div class="form-floating">
-        <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="login">
+        <Field name="login" rules="input-text-rule" :class="errors.login" class="form-control" id="floatingInput"
+               v-model="login"/>
         <label for="floatingInput">Email address</label>
       </div>
       <div class="form-floating">
-        <input type="text" class="form-control" id="floatingPassword" placeholder="Password" v-model="password">
+        <Field name="password" rules="input-text-rule" :class="errors.password" class="form-control"
+               id="floatingPassword" v-model="password"/>
         <label for="floatingPassword">Password</label>
       </div>
 
+
       <button class="w-100 btn btn-lg btn-primary" @click="authFarmer">Sign in</button>
-    </form>
+    </Form>
   </main>
 
 </template>
 
 <script>
-import objDeviceUUID from "/node_modules/device-uuid/lib/device-uuid.js"
+import {Form, Field} from 'vee-validate';
+
+// import objDeviceUUID from "/node_modules/device-uuid/lib/device-uuid.js"
 
 export default {
   name: "ComponentAuth",
+  components: {
+    Form,
+    Field,
+  },
   inject: ["globalVariables"],
   data() {
     return {
@@ -37,33 +41,33 @@ export default {
     }
   },
   mounted() {
-    if (this.innerGlobalVariables.isAuthorized === false) {
-
-      let deviceId = new objDeviceUUID.DeviceUUID().get()
-
-      fetch("http://localhost:8090/mobile/apps/authByLoginAndPassword", {
-        method: "post",
-        headers: {
-          "Content-type": "application/json"
-        },
-        body: JSON.stringify({
-          login: "web",
-          password: "12345",
-          deviceId: deviceId
-        })
-      }).then(response => response.json()).then(responseAsObject => {
-        console.dir("success auth");
-
-        this.innerGlobalVariables.httpHeaders.API_KEY = responseAsObject.apiKey;
-        this.innerGlobalVariables.httpHeaders.DEVICE_ID = deviceId;
-        this.innerGlobalVariables.isAuthorized = true;
-
-        console.dir(this.innerGlobalVariables.httpHeaders);
-
-      }).catch(error => {
-        console.log('Looks like there was a problem: \n', error);
-      });
-    }
+    // if (this.innerGlobalVariables.isAuthorized === false) {
+    //
+    //   let deviceId = new objDeviceUUID.DeviceUUID().get()
+    //
+    //   fetch("http://localhost:8090/mobile/apps/authByLoginAndPassword", {
+    //     method: "post",
+    //     headers: {
+    //       "Content-type": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //       login: "web",
+    //       password: "12345",
+    //       deviceId: deviceId
+    //     })
+    //   }).then(response => response.json()).then(responseAsObject => {
+    //     console.dir("success auth");
+    //
+    //     this.innerGlobalVariables.httpHeaders.API_KEY = responseAsObject.apiKey;
+    //     this.innerGlobalVariables.httpHeaders.DEVICE_ID = deviceId;
+    //     this.innerGlobalVariables.isAuthorized = true;
+    //
+    //     console.dir(this.innerGlobalVariables.httpHeaders);
+    //
+    //   }).catch(error => {
+    //     console.log('Looks like there was a problem: \n', error);
+    //   });
+    // }
   },
   methods: {
     authFarmer() {
@@ -74,5 +78,21 @@ export default {
 </script>
 
 <style scoped>
+.input-not-valid {
+  outline: none !important;
+  box-shadow: none !important;
 
+  border-style: solid !important;
+  border-width: 1px;
+  border-color: red !important;
+}
+
+.input-valid {
+  outline: none !important;
+  box-shadow: none !important;
+
+  border-style: solid !important;
+  border-width: 1px;
+  border-color: green !important;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,23 @@ import "bootstrap"
 
 import BootstrapIcon from '@dvuckovic/vue3-bootstrap-icons';
 
+import {defineRule, configure} from 'vee-validate';
+
+configure({
+    validateOnBlur: true, // controls if blur events should trigger validation with handleChange handler
+    validateOnChange: true, // controls if change events should trigger validation with handleChange handler
+    validateOnInput: true, // controls if input events should trigger validation with handleChange handler
+    validateOnModelUpdate: true, // controls if update:modelValue events should trigger validation with handleChange handler,
+});
+
+defineRule("input-text-rule", value => {
+    if (!value || !value.length) {
+        return "input-not-valid";
+    } else {
+        return "input-valid";
+    }
+});
+
 createApp(App)
     .provide("globalVariables", globalVariables)
     .use(router)
