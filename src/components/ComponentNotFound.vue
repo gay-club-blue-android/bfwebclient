838a75356879d6a0d8c24956471b9564ce40dc8f
Hades-des <chornnegr@gmail.com>
2022-07-05 20:30:31 +0300

    add notFound page

--- src/components/ComponentNotFound.vue
+++ src/components/ComponentNotFound.vue
@@ -1,13 +1,36 @@
 <template>
-  <h2>oops resource not found</h2>
+  <div class="text">
+  <h2 >Страница не найдена</h2>
+  </div>
+  <div class="back">
+   <h3> Вернуться назад</h3>
+  </div>
+  <div class="background"></div>
 </template>
 
 <script>
 export default {
   name: "ComponentNotFound"
 }
+
 </script>
 
 <style scoped>
+ body{
+    background: #1A1919 url("/public/notFound/images/Background.svg") ;
+    width: auto;
+    height: auto;
+ }
+ .text{
+   color: #ffffff;
+ }
+ .back{
+   color: #ffffff;
+   cursor: pointer;
+ }
+ .back:hover
+ {
+   transform: scale(1.5);
+ }
 
 </style>
